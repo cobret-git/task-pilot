95d31e291fd0a6e15d2354933c575efb49017c39
Kostia Britikov <konstantin.britikov@gmail.com>
2025-12-09 20:04:35 +0100

    Update project configurations
    TaskPilot.Core:
    - Remove empty folder references from project file
    
    TaskPilot.Desktop.WinApp:
    - Set AssemblyName to TaskPilot
    - Add Services folder reference
    
    App.xaml.cs:
    - Add Environment.CurrentDirectory access on launch

--- src/TaskPilot.Desktop.WinApp/App.xaml.cs
+++ src/TaskPilot.Desktop.WinApp/App.xaml.cs
@@ -43,6 +43,7 @@ namespace TaskPilot.Desktop.WinApp
         /// <param name="args">Details about the launch request and process.</param>
         protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
         {
+            _  = Environment.CurrentDirectory;
             _window = new MainWindow();
             _window.Activate();
         }
